c2a631c722afadda6947ce9360b6cc7f879fe503
Omkar Patil <omkarp666@gmail.com>
2023-05-16 12:52:06 +0530

    added store and homeSlice using redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
   useEffect(() => {
     apiTest();
   }, []);
@@ -9,10 +13,15 @@ function App() {
   const apiTest = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      App
+      {url?.total_pages}
+    </div>
+  );
 }
 
 export default App;
- 
