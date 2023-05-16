ba399642dbb7b079de814a2c4de46e2b0a548696
Omkar Patil <omkarp666@gmail.com>
2023-05-16 15:56:35 +0530

    completed footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import { FaFacebookF, FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 import "./style.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const githubProfileUrl = "https://github.com/omkarp887";
+  const linkedinUrl = "https://www.linkedin.com/in/omkar-patil-227387218/";
+
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">Created By Omkar Patil.</div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <a
+              href={githubProfileUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
